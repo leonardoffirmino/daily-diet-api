d3356778f4fc018ed9b29fff767343cbdcd31518
Leonardo <firmino750@gmail.com>
2025-03-10 23:12:25 -0300

    Create route get all item and refactor route the create meal

--- app.py
+++ app.py
@@ -20,25 +20,25 @@ def load_user(user_id):
   return User.query.get(int(user_id))
 
   
-#View de login para autenticação 
+#Route for create new user in api 
 @app.route("/login", methods=["POST"])
 def login():
   data = request.json
   username = data.get("username")
   password = data.get("password")
+  name = data.get("name")
 
   if username and password:
     #Login
-    user = User.query.filter_by(username=username).first()
-
-    if user:
-    # Autenticado com sucesso
-      print("Ok")
+      user = User(username=username,password=password,name=name)
+      #Insert data the database 
+      db.session.add(user)
+      db.session.commit()
       return jsonify({"message":"Autenticação realizada com sucesso"})
 
-    return jsonify({"message":"Falha de autenticação!!"})
+    #return jsonify({"message":"Falha de autenticação!!"})
   
-  return jsonify({"message":"Credentials invalid!"}),400
+  return jsonify({"error":"Credentials invalid!"}),400
 
 @app.route("/logout", methods=["GET"])
 @login_required
@@ -51,22 +51,41 @@ def logout():
 @app.route("/meal",methods=["POST"])
 def create_meal():
   data = request.json
+
+  # Verifica se user_id foi enviado
+  if "user_id" not in data:
+    return jsonify({"error": "User ID is required"}), 400
+
+  # Verifica se o usuário existe
+  user = User.query.get(data["user_id"])
+  if not user:
+    return jsonify({"error": "User not found"}), 404
+    
+ 
   name_meal = data.get("name_meal")
   description_meal = data.get("description_meal")
   is_on_diet = data.get("is_on_diet")
+  user_id=user.id 
 
   if name_meal and description_meal:
-    new_meal = Meal(meal=name_meal, description=description_meal, is_on_diet=is_on_diet)
+    new_meal = Meal(meal=name_meal, description=description_meal, is_on_diet=is_on_diet,user_id=user_id)
     #Insert data the database 
     db.session.add(new_meal)
     db.session.commit()
     return jsonify({'message': "Meal created sucessfully!"})
   
-  return jsonify({'message': "Data invalid"})
+  return jsonify({'error': "Data invalid"})
 
-@app.route("/get_meal",methods=["GET"])
-def get_meal():
-  return 
+@app.route("/get_meal/<int:id_user>",methods=["GET"])
+def get_meal(id_user):
+  user_id = User.query.get(id_user)
+
+  if user_id:
+    meals = Meal.query.filter_by(user_id=user_id).all()
+    
+    return jsonify({"Username return --> ":user_id.username},meals)
+  
+  return jsonify({'error':"User not found!"}),404
   
 
   
